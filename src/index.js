db9f7d24837af1de45677b2de07d16bb7d85f8a7
maximes2004 <maximes2004@gmail.com>
2026-05-07 22:23:45 +0400

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return [];
+  }
+  return matrix.flatMap((el, i) => (i % 2 !== 0 ? el.reverse() : el));
 };
